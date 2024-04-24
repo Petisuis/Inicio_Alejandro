715a4b1b1d684bd5133e2c035d738832f713feee
aleja <aleja@LAPTOP-IMPQVJ86>
2024-04-24 11:51:12 +0200

    Mengueche

---
+++ RepositorioTarea/src/Entornos/Hola.java
@@ -0,0 +1,23 @@
+package Entornos;
+
+import java.util.Scanner;
+
+public class Hola {
+	public static void main(String [] arg) {
+		
+		Scanner ent=new Scanner(System.in);
+		
+		
+		System.out.println("Mengueche");
+		
+		int rezos=Integer.parseInt(ent.nextLine());
+		
+		for(int i=0; i <rezos; i++) {
+		System.out.println("Mengueche");
+		}
+	}
+
+}
+
+
+
